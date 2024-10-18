7aaf1788fb065e90abe9e218fbc6a2746054f829
escapeuw <ghkdehd78999@gmail.com>
2024-10-18 00:58:29 -0400

    responsive version 1

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -19,43 +19,47 @@ function About() {
     return (
         <section id="about">
             <div className="aboutContainer">
-            <div className="typeContainer">
-                <div className="typeIt">
-                    <TypeIt style={{ fontSize: "35px" }}
-                        options={{
-                            strings: ["Hello, my name is <a>Daniel</a>.<br/> I'm a junior Front End Developer."],
-                            speed: 100,
-                            loop: true,
-                            loopDelay: [1000, 4000],
-                            waitUntilVisible: true,
-                            lifeLike: true
-                        }} />
+                <div className="typeContainer">
+                    <div className="typeIt">
+                        <TypeIt style={{ fontSize: "35px" }}
+                            options={{
+                                strings: ["Hello, my name is <a>Daniel</a>.<br/> I'm a junior Front End Developer."],
+                                speed: 100,
+                                loop: true,
+                                loopDelay: [1000, 4000],
+                                waitUntilVisible: true,
+                                lifeLike: true
+                            }} />
+                    </div>
+                    <div className="profile">
+                        <div>A <span className="underline">creative</span> developer with exceptional experience
+                            in <span className="underline">frontend</span> development. I'm also very interested and passionate about learning backend architectures.</div>
+                        <div>Turning <span className="underline">ideas</span> into real life <span className="underline">products</span> is my calling.</div>
+                    </div>
                 </div>
-                <div className="profile">
-                    <div>A <span className="underline">creative</span> developer with exceptional experience
-                        in <span className="underline">frontend</span> development. I'm also very interested and passionate about learning backend architectures.</div>
-                    <div>Turning <span className="underline">ideas</span> into real life <span className="underline">products</span> is my calling.</div>
+                <div className="resumeContainer">
+                    <a href={resume} target="blank" style={{ textDecoration: "none" }}><p className="resume">View Resume</p></a>
                 </div>
-            </div>
-            <div className="resumeContainer">
-                <a href={resume} target="blank" style={{textDecoration:"none"}}><p className="resume">View Resume</p></a>
-            </div>
-        
-            <div className="dock">
-                <div className="dockIcon"><div className="iconContainer"><div className="iconName">Html</div><img src={htmlIcon} /></div></div>
-                <div className="dockIcon"><div className="iconContainer"><div className="iconName" style={{left:"10px"}}>CSS</div><img src={css3Icon} /></div></div>
-                <div className="dockIcon"><div className="iconContainer"><div className="iconName">SASS</div><img src={sassIcon} /></div></div>
-                <div className="dockIcon"><div className="iconContainer"><div className="iconName" style={{left:"-8px"}}>BOOTSTRAP</div><img src={bsIcon} /></div></div>
-                <div className="dockIcon"><div className="iconContainer"><div className="iconName" style={{left:"0"}}>PYTHON</div><img src={python2Icon} /></div></div>
-                <div className="dockIcon"><div className="iconContainer"><div className="iconName" style={{left:"-3px"}}>JavaScript</div><img src={jsIcon} /></div></div>
-                <div className="dockIcon"><div className="iconContainer"><div className="iconName" style={{left:"-3px"}}>TypeScript</div><img src={tsIcon} /></div></div>
-                <div className="dockIcon"><div className="iconContainer"><div className="iconName">React</div><img src={reactIcon} /></div></div>
-                <div className="dockIcon"><div className="iconContainer"><div className="iconName" style={{left:"5px"}}>Redux</div><img src={reduxIcon} /></div></div>
-                <div className="dockIcon"><div className="iconContainer"><div className="iconName" style={{left:"15px"}}>C</div><img src={cIcon} /></div></div>
-                <div className="dockIcon"><div className="iconContainer"><div className="iconName">JSON</div><img src={jsonIcon} /></div></div>
-                <div className="dockIcon"><div className="iconContainer"><div className="iconName">jQuery</div><img src={jqueryIcon} /></div></div>
-                <div className="dockIcon"><div className="iconContainer"><div className="iconName" style={{left:"15px"}}>R</div><img style={{height:"90%", objectFit:"fill", marginTop:"3px"}} src={rIcon} /></div></div>
-            </div>
+
+                <div className="dock">
+                    <div className="dockIcon">
+                        <div className="iconContainer">
+                            <div className="iconName">Html</div>
+                            <img src={htmlIcon} /></div>
+                    </div>
+                    <div className="dockIcon"><div className="iconContainer"><div className="iconName" style={{ left: "10px" }}>CSS</div><img src={css3Icon} /></div></div>
+                    <div className="dockIcon"><div className="iconContainer"><div className="iconName">SASS</div><img src={sassIcon} /></div></div>
+                    <div className="dockIcon"><div className="iconContainer"><div className="iconName" style={{ left: "-8px" }}>Bootstrap</div><img src={bsIcon} /></div></div>
+                    <div className="dockIcon"><div className="iconContainer"><div className="iconName" style={{ left: "0" }}>Python</div><img src={python2Icon} /></div></div>
+                    <div className="dockIcon"><div className="iconContainer"><div className="iconName" style={{ left: "-3px" }}>JavaScript</div><img src={jsIcon} /></div></div>
+                    <div className="dockIcon"><div className="iconContainer"><div className="iconName" style={{ left: "-3px" }}>TypeScript</div><img src={tsIcon} /></div></div>
+                    <div className="dockIcon"><div className="iconContainer"><div className="iconName">React</div><img src={reactIcon} /></div></div>
+                    <div className="dockIcon"><div className="iconContainer"><div className="iconName" style={{ left: "5px" }}>Redux</div><img src={reduxIcon} /></div></div>
+                    <div className="dockIcon"><div className="iconContainer"><div className="iconName" style={{ left: "15px" }}>C</div><img src={cIcon} /></div></div>
+                    <div className="dockIcon"><div className="iconContainer"><div className="iconName">JSON</div><img src={jsonIcon} /></div></div>
+                    <div className="dockIcon"><div className="iconContainer"><div className="iconName">jQuery</div><img src={jqueryIcon} /></div></div>
+                    <div className="dockIcon"><div className="iconContainer"><div className="iconName" style={{ left: "15px" }}>R</div><img style={{ height: "90%", objectFit: "fill", marginTop: "3px" }} src={rIcon} /></div></div>
+                </div>
             </div>
         </section>
     );
