fefe7cd61865b303d7fafdf0df6babf971e550ec
escapeuw <ghkdehd78999@gmail.com>
2024-10-24 22:58:09 -0400

    contact page update #1

--- src/contact/contact.jsx
+++ src/contact/contact.jsx
@@ -3,7 +3,14 @@ import './contact.css';
 function Contact() {
     return (
         <section id="contact">
-            <p>contact page view</p>
+            <div className="contactContainer">
+                <div className="contactItem"><i class="fa-brands fa-github"></i></div>
+                <div className="contactItem"><i class="fa-brands fa-linkedin"></i></div>
+                <div className="email">
+                    <span><i class="fa-solid fa-envelope"></i> d.hwang0918@gmail.com</span>
+                    <span className="copyButton">copy</span>
+                </div>
+            </div>
         </section>
     )
 }
