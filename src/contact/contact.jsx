59b0151c97e652fb7b1eb1d4517f0ccd7f52e83f
escapeuw <ghkdehd78999@gmail.com>
2024-10-25 20:17:28 -0400

    mostly done!

--- src/contact/contact.jsx
+++ src/contact/contact.jsx
@@ -4,12 +4,33 @@ function Contact() {
     return (
         <section id="contact">
             <div className="contactContainer">
-                <div className="contactItem"><i class="fa-brands fa-github"></i></div>
-                <div className="contactItem"><i class="fa-brands fa-linkedin"></i></div>
-                <div className="email">
-                    <span><i class="fa-solid fa-envelope"></i> d.hwang0918@gmail.com</span>
-                    <span className="copyButton">copy</span>
-                </div>
+                <a href="https://github.com/escapeuw" target="blank" style={{ textDecoration: "none", cursor: "default" }}>
+                    <div className="contactItem">
+                        <i class="fa-brands fa-github"></i>
+                    </div>
+                </a>
+                <a href="https://www.linkedin.com/in/dhwang2" target="blank" style={{ textDecoration: "none", cursor: "default" }}>
+                    <div className="contactItem">
+                        <i class="fa-brands fa-linkedin"></i>
+                    </div>
+                </a>
+                <a href="https://www.instagram.com/escapeuw" target="blank" style={{ textDecoration: "none", cursor: "default" }}>
+                    <div className="contactItem">
+                        <i class="fa-brands fa-instagram"></i>
+                    </div>
+                </a>
+            </div>
+
+            <div className="email" onClick={() => {
+                navigator.clipboard.writeText('d.hwang0918@gmail.com');
+                alert("Copied to clipboard!")
+            }}>
+                <span>d.hwang0918@gmail.com</span>
+                <span className="vl"></span>
+                <span>Copy</span>
+            </div>
+            <div className="copyright">
+                <p>Daniel Hwang <span>©2024</span></p>
             </div>
         </section>
     )
